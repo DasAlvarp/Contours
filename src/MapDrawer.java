a49f6784233135a149699fc16911e311c38356e3
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-10-10 14:03:45 -0500

    MapDrawer added

---
+++ src/MapDrawer.java
@@ -0,0 +1,15 @@
+import com.jogamp.opengl.awt.GLCanvas;
+
+import javax.swing.*;
+
+/**
+ * Created by alvarpq on 10/5/2016.
+ */
+public class MapDrawer extends JFrame
+{
+    private GLCanvas canvas;
+    public MapDrawer()
+    {
+
+    }
+}
